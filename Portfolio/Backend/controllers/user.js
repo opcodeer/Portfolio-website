14d8b9e847e13536717714c1bbc244c03bdcf913
gautam gautam <gautamdtua42003@gmail.com>
2024-07-05 18:52:54 +0700

    changing some image urls

--- Portfolio/Backend/controllers/user.js
+++ Portfolio/Backend/controllers/user.js
@@ -1,19 +1,12 @@
-// require('dotenv').config();
+require('dotenv').config();
 // const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
-JWT_SECRET = 'mySecretKeyForJWTs@2024'
+JWT_SECRET = process.env.JWT_SECRET || 'mySecretKeyForJWTs@2024';
 const getmessage = async (req, res) => {
     const { name,email,message } = req.body;
     
     try {
-        // const existingUser = await User.findOne({ email });
-        // if (existingUser) return res.status(400).json({ message: 'User already exists.' });
-        
-        // if (password !== confirmPassword) return res.status(400).json({ message: 'Passwords don\'t match' });
-        
-        // const hashedPassword = await bcrypt.hash(password, 12);
-        
         const user = await User.create({ name: name, email:email, message:message });
         
         const authToken = jwt.sign({ email: user.email, id: user._id }, JWT_SECRET, { expiresIn: '1h' });
