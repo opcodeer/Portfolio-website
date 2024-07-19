75526adcba95943d8101445484792e3c9c12255a
gautam gautam <gautamdtua42003@gmail.com>
2024-07-19 18:13:15 +0700

    Refactoring vite.config.js file

--- Portfolio/vite.config.js
+++ Portfolio/vite.config.js
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css:{
-    modules:{
-      localsConvention:"camelCase",
+  css: {
+    modules: {
+      localsConvention: "camelCase",
     },
   },
+  server: {
+    host: '0.0.0.0', // This makes the server accessible externally
+    port: 3000 // Specify the port you want to use
+  }
 });
+
