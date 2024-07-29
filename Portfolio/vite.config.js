12b5ff384dd7d522653e72b2d66c091184fdb7fc
gautam gautam <gautamdtua42003@gmail.com>
2024-07-29 17:36:39 +0700

    Adding NODE_ENV variable in env file

--- Portfolio/vite.config.js
+++ Portfolio/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
@@ -10,10 +9,11 @@ export default defineConfig({
     },
   },
   server: {
-    host: '0.0.0.0', // This makes the server accessible externally
-    port: 5000 // Specify the port you want to use
+    host: '0.0.0.0',
+    port: 5000,
   },
   define: {
-    'process.env.NODE_ENV': JSON.stringify('production')
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production')
   }
 });
+
