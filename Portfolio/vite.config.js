041fa74c4bff3e077f6410688914bf32a38dc5d6
gautam gautam <gautamdtua42003@gmail.com>
2024-07-29 17:53:36 +0700

    Refactoring production code of my portfolio

--- Portfolio/vite.config.js
+++ Portfolio/vite.config.js
@@ -1,19 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { terser } from 'rollup-plugin-terser';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    terser() // Ensure minification for production
+  ],
   css: {
     modules: {
-      localsConvention: "camelCase",
+      localsConvention: 'camelCase',
     },
   },
-  server: {
-    host: '0.0.0.0',
-    port: 5000,
+  build: {
+    minify: 'terser', // Enable Terser for minification
+    sourcemap: false, // Disable source maps for production builds
   },
   define: {
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production')
-  }
+    'process.env.NODE_ENV': JSON.stringify('production')
+  },
 });
-
