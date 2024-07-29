5af4db3607bda51c5687e9a2382a69b3d86f68e3
gautam gautam <gautamdtua42003@gmail.com>
2024-07-29 18:07:32 +0700

    refactoring vite.config.js

--- Portfolio/vite.config.js
+++ Portfolio/vite.config.js
@@ -1,26 +1,26 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { terser } from 'rollup-plugin-terser';
+import replace from '@rollup/plugin-replace';
 
 export default defineConfig({
   plugins: [
     react(),
-    terser() // Ensure minification for production
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+      preventAssignment: true,
+    }),
   ],
   css: {
     modules: {
       localsConvention: 'camelCase',
     },
   },
-  server: {
-    host: '0.0.0.0',
-    port: 5000,
-  },
   build: {
     minify: 'terser', // Enable Terser for minification
     sourcemap: false, // Disable source maps for production builds
-  },
-  define: {
-    'process.env.NODE_ENV': JSON.stringify('production')
+    rollupOptions: {
+      plugins: [terser()], // Ensure minification
+    },
   },
 });
